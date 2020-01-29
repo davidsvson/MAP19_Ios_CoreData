71d9a6ff161a04a891618c8f2ed23a3f29426dc7
David Svensson <davidsvson@gmail.com>
2020-01-29 10:43:31 +0100

    alertController added

--- CoreDataLesson/ViewController.swift
+++ CoreDataLesson/ViewController.swift
@@ -8,13 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource{
+    
+    let cellId = "cellId"
+    
+    var items = ["gurka", "ost", "majonäs"]
+    
+    @IBOutlet weak var itemsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        itemsTableView.dataSource = self
     }
 
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        
+        cell.textLabel?.text = items[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    @IBAction func addItem(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "New Item", message: "Add a new Item", preferredStyle: .alert)
+        
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) { action in
+            
+            guard let textField = alert.textFields?.first else {return}
+            guard let itemToSave = textField.text else {return}
+            
+            self.items.append(itemToSave)
+            self.itemsTableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alert.addTextField()
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+      
+        
+        present(alert, animated: true)
+    }
+    
 }
 
