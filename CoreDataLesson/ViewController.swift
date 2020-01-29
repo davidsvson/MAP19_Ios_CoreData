109bd3ccbcd7868550ebcccf959c3019660e23ad
David Svensson <davidsvson@gmail.com>
2020-01-29 09:47:55 +0100

    Initial Commit

---
+++ CoreDataLesson/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataLesson
+//
+//  Created by David Svensson on 2020-01-29.
+//  Copyright © 2020 David Svensson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
